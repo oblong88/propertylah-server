0e4ac95a3dda5725a23e0b80ae16fb6343ec562c
Juzantino Junadi <juzantinojunadi@Juzantinos-MacBook-Pro.local>
2022-03-06 11:56:04 +0800

    update

---
+++ models/questionModel.js
@@ -0,0 +1,48 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("../models/db");
+
+class Question extends Model {}
+
+Question.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "first_name",
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: "last_name",
+    },
+    question: {
+      type: DataTypes.STRING,
+        allowNull: false,
+      field: 'question'
+        },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      field: "created_at",
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      field: "updated_at",
+    },
+  },
+  {
+    sequelize,
+    modelName: "Question",
+    tableName: "question",
+  }
+);
+
+// TODO: create associations
+
+module.exports = Question;

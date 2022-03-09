c665eb1b97e88cf44b9bad6cffab1e43c74ebaaa
Xin Kai <tam.xin.kai@gmail.com>
2022-03-09 13:50:30 +0800

    test2

--- models/db.js
+++ models/db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.NODE_ENV === "development"
     ? process.env.DEV_DB_USERNAME
     : process.env.PROD_DB_USERNAME,
-  "X@n@th0s",
+  "ZwSSNA2Mo#wyXY",
   {
     host: "localhost",
     dialect: "postgres",

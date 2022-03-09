d1b15e8c67ab8e0c090a46f40986cccfaa2e3f20
thinkdaniel <danielgoh@outlook.com>
2022-03-09 09:39:59 +0800

    updated db.js for heroku postgres

--- models/db.js
+++ models/db.js
@@ -1,20 +1,42 @@
 const { Sequelize } = require("sequelize");
 
 // DB connection
-const sequelize = new Sequelize(
-  process.env.NODE_ENV === "development"
-    ? process.env.DEV_DB_NAME
-    : process.env.PROD_DB_NAME,
-  process.env.NODE_ENV === "development"
-    ? process.env.DEV_DB_USERNAME
-    : process.env.PROD_DB_USERNAME,
-  "postgres",
-  {
+
+let sequelize;
+
+if (process.env.NODE_ENV === "development")
+  sequelize = new Sequelize(
+    process.env.DEV_DB_NAME,
+    process.env.DEV_DB_USERNAME,
+    "postgres",
+    {
+      host: "localhost",
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+
+if (process.env.NODE_ENV === "production")
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: "localhost",
     dialect: "postgres",
     logging: false,
-  }
-);
+  });
+
+// const sequelize = new Sequelize(
+//   process.env.NODE_ENV === "development"
+//     ? process.env.DEV_DB_NAME
+//     : process.env.PROD_DB_NAME,
+//   process.env.NODE_ENV === "development"
+//     ? process.env.DEV_DB_USERNAME
+//     : process.env.PROD_DB_USERNAME,
+//   "postgres",
+//   {
+//     host: "localhost",
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
 
 // test connection function
 const testConnection = async () => {

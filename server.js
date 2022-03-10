e15785764caa8622344e177ba6857cad1ea08fde
thinkdaniel <danielgoh@outlook.com>
2022-03-10 08:29:26 +0800

    removed arg from sequelize.sync

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const startServer = () => {
 
 // sync db regardless of env
 sequelize
-  .sync({ alter: true })
+  .sync()
   .then(startServer)
   .catch((err) =>
     console.error("[server.js] ❌ Database error: ", err.message)
